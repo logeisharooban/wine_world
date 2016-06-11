7937f1024cc6abc0f38c54a09095b0198189d1f4
Rooban <rooban@app-monkeyz.com>
2016-06-11 17:33:12 +0530

    initial test commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $("#main-slider").owlCarousel({
+	  nav : false,
+	  items : 1,
+	  pagination : false,
+  });//Main slider
+  
+  $("#popular-products-slider").owlCarousel({
+    margin: 20,
+    nav : false,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    },
+    pagination : true,
+  });//Testimonials slider
+
+  $("#drag-slider").slider({});
+
+  $('ul.nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+  }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+  });
+    
+});
